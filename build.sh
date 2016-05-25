cf900808cb14b9add245ff367d5d414353f4a54c
david <david.sanchez.falero@gmail.com>
2016-05-25 09:46:42 +0200

    jenkins using docker-compose

--- build.sh
+++
@@ -1,2 +0,0 @@
-docker build -t jenkins-data -f Dockerfile-data .
-docker build -t jenkins2 .
--- run.sh
+++
@@ -1,2 +0,0 @@
-docker run --name=jenkins-data jenkins-data
-docker run -p 8080:8080 -p 50000:50000 --name=jenkins-master --volumes-from=jenkins-data -d jenkins2
